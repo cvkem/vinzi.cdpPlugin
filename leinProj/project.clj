da0d9ec3ba8ee00f1edfca51d42dd4f9ef3ecf34
C. van Kemenade <cde@vinzi.nl>
2014-02-13 03:41:42 +0100

    Fixed project to make small uberjar
    1. Excluding dev-jars
    2. compiling java

--- leinProj/project.clj
+++ leinProj/project.clj
@@ -2,24 +2,48 @@
  vinzi/vinzi.cdpPlugin
  "0.2.0-SNAPSHOT"
  :dependencies
- [[org.clojure/clojure "1.5.1"]
-  [org.slf4j/slf4j-api "1.6.5"]
+ [[org.slf4j/slf4j-api "1.6.5"]
   [ch.qos.logback/logback-core "1.0.6"]
   [ch.qos.logback/logback-classic "1.0.6"]
-  [commons-codec "1.3"]
-  [commons-logging "1.1.1"]
-  [javax.servlet/servlet-api "2.3"]
-  [xerces/xercesImpl "2.9.1"]
-  [pentaho/pentaho-bi-platform-api "3.9.0-stable"]
-  [pentaho/pentaho-bi-platform-engine-core "3.9.0-stable"]
-  [pentaho/pentaho-bi-platform-engine-services "3.9.0-stable"]
-  [pentaho/pentaho-bi-platform-util "3.9.0-stable"]
-  [dom4j "1.6.1"]
-  [commons-lang "2.4"]
   [vinzi/vinzi.tools "0.2.0-SNAPSHOT"]
   [vinzi/vinzi.pentaho "0.2.0-SNAPSHOT"]
   [vinzi/vinzi.data "0.1.0-SNAPSHOT"]
   [vinzi/vinzi.cdp "0.2.0-SNAPSHOT"]]
+
+
+;                   :exclusions [org.clojure/clojure
+;                                org.postgresql/postgresql
+;                                commons-codec/commons-codec
+;                                javax.servlet/servlet-api
+;                                 ]
+  :uberjar-exclusions [#"^javax/"
+                       #"^clojure/lang/"
+                       #"^org/postgresql/"
+                       #"^org/pentaho/"
+                        #"^org/apache/"
+                       #"^org/springframework/"
+                       #"^org/codehaus/"
+                       #"^org/codehaus/"
+                       #"^org/jaxen/"
+                       #"^org/w3c/"
+                       #"^org/dom4j/"
+                       #"^org/aopalliance/"
+                       ]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [commons-codec "1.3"]
+                                  [commons-logging "1.1.1"]
+                                  [javax.servlet/servlet-api "2.3"]
+                                  [xerces/xercesImpl "2.9.1"]
+                                  [pentaho/pentaho-bi-platform-api "3.9.0-stable"]
+                                  [pentaho/pentaho-bi-platform-engine-core "3.9.0-stable"]
+                                  [pentaho/pentaho-bi-platform-engine-services "3.9.0-stable"]
+                                  [pentaho/pentaho-bi-platform-util "3.9.0-stable"]
+                                  [dom4j "1.6.1"]
+                                  [commons-lang "2.4"]
+                                  ]
+                   }}
+ :java-source-paths ["javaSrc"]
  :deploy-repositories
  {:snapshots "no distrib-mgt", :releases "no distrib-mgt"}
  :repositories
